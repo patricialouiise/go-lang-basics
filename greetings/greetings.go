a12b698646a10ca239178f9a8e3b4f62d5b36714
Patricia Louise Ramos <ramospatricialouise@gmail.com>
2024-03-12 14:01:52 +0800

    Initial commit

---
+++ greetings/greetings.go
@@ -0,0 +1,19 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Hello returns a greeting for the named person.
+// Go exported name
+func Hello(name string) (string, error) {
+		// If no name was given, return an error with a message.
+		if name == "" {
+			return "", errors.New("empty name")
+		}
+    
+		// Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    return message, nil
+}
